814a05d3545aedc7693bc30985762b377213a341
Christopher Shea <Chris@Christophers-MacBook-Pro.local>
2016-06-07 15:50:54 -0400

    Implement eraser brush

--- CanvasView.swift
+++ CanvasView.swift
@@ -9,28 +9,48 @@
 import UIKit
 
 class CanvasView: UIImageView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
     
-    var brushColor = UIColor.blackColor()
+    /*
+     // Only override drawRect: if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func drawRect(rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+    var brushColor = UIColor.blackColor() { didSet { previousBrushColor = oldValue } }
+    var previousBrushColor = UIColor.blackColor()
+    var pendingBrushColor: UIColor?
+    var isErasing = false {
+        willSet {
+            switch newValue {
+            case true:
+                brushColor = self.backgroundColor!
+            case false:
+                if let pendingColor = pendingBrushColor {
+                    brushColor = pendingColor
+                    pendingBrushColor = nil
+                } else {
+                    brushColor = previousBrushColor
+                }
+            }
+        }
+    }
+    
+    var brushSize: CGFloat = 20.0
     
     func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
         
         UIGraphicsBeginImageContext(self.bounds.size)
         let context = UIGraphicsGetCurrentContext()
+        
         self.image?.drawInRect(CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
         
         CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
         CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
         
         CGContextSetLineCap(context, .Round)
-        CGContextSetLineWidth(context, 5.0)
+        CGContextSetLineWidth(context, brushSize)
         CGContextSetStrokeColorWithColor(context, brushColor.CGColor)
         CGContextSetBlendMode(context, .Normal)
         
@@ -39,8 +59,7 @@ class CanvasView: UIImageView {
         self.image = UIGraphicsGetImageFromCurrentImageContext()
         self.alpha = 1.0
         
-       UIGraphicsEndImageContext()
-        
+        UIGraphicsEndImageContext()
     }
-
+    
 }
--- Paint/CanvasViewController.swift
+++ Paint/CanvasViewController.swift
@@ -12,6 +12,7 @@ class CanvasViewController: UIViewController {
     
     @IBOutlet weak var canvasView: CanvasView!
     @IBOutlet var colorPalette: [UIButton]!
+    @IBOutlet var brushSizePalette: [UIButton]!
     
     var lastPoint = CGPoint.zero
     var swiped = false
@@ -22,34 +23,57 @@ class CanvasViewController: UIViewController {
         for color in colorPalette {
             color.hidden = true
         }
+        
+        for brush in brushSizePalette {
+            brush.hidden = true
+        }
     }
-    
-    
     
     
     //MARK: IBActions
     @IBAction func toggleColorPalette(sender: UIButton) {
         let isHidden = colorPalette.first?.hidden
-        
         for color in colorPalette {
             color.hidden = !isHidden!
         }
     }
     
-    @IBAction func setBrushColor(sender: UIButton) {
-        canvasView.brushColor = sender.backgroundColor!
+    @IBAction func toggleBrushSizePalette(sender: UIButton) {
+        let isHidden = brushSizePalette.first?.hidden
+        for size in brushSizePalette {
+            size.hidden = !isHidden!
+        }
     }
-
+    
+    @IBAction func toggleEraser(sender: UIButton) {
+        let isSelected = sender.selected
+        sender.selected = !isSelected
+        canvasView.isErasing = sender.selected
+    }
+    
+    @IBAction func setBrushColor(sender: UIButton) {
+        let selectedColor = sender.backgroundColor!
+        if canvasView.isErasing {
+            canvasView.pendingBrushColor = selectedColor
+        } else {
+            canvasView.brushColor = selectedColor
+        }
+    }
+    
+    @IBAction func setBrushSize(sender: UIButton) {
+        let size = (sender.titleLabel?.font.pointSize)!
+        canvasView.brushSize = size
+    }
+    
     
     //MARK: Touch Events
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         swiped = false
-        
         if let touch = touches.first {
             lastPoint = touch.locationInView(canvasView)
         }
     }
-
+    
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         swiped = true
@@ -63,7 +87,7 @@ class CanvasViewController: UIViewController {
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if !swiped {
-           canvasView.drawLineFrom(lastPoint, toPoint: lastPoint)
+            canvasView.drawLineFrom(lastPoint, toPoint: lastPoint)
         }
     }
     
